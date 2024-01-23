dee8ab78f9d0792c7fa2b403d7208120dc726d54
rxwyun <1120309488@qq.com>
2024-01-23 16:44:46 +0800

    merge

--- src/database/factory.go
+++ src/database/factory.go
@@ -6,5 +6,6 @@ import (
 
 type Factory interface {
 	Channel(channel string) (*gorm.DB, error)
+	RegisterDriver(driver string, resolver func(config Config) (gorm.Dialector, error))
 	RegisterDb(channel string, dbResolver func() (*gorm.DB, error))
 }
--- src/logger/factory.go
+++ src/logger/factory.go
@@ -6,5 +6,6 @@ import (
 
 type Factory interface {
 	Channel(channel string) (*zap.Logger, error)
+	RegisterDriver(driver string, resolver func(config Config) (Driver, error))
 	RegisterLogger(channel string, loggerResolver func() (*zap.Logger, error))
 }
